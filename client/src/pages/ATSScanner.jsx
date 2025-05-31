9b5da55d859a2d31e44eb0a3658db3f944e7a051
louis nguyen <louis.nguyen550@gmail.com>
2025-05-31 16:24:18 -0400

    creating folder structure and simple UI

---
+++ client/src/pages/ATSScanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ATSScanner = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 p-8">
+      <h1 className="text-3xl font-bold mb-6">ATS Scanner</h1>
+      {/* ATS scanning interface will go here */}
+    </div>
+  );
+};
+
+export default ATSScanner; 
---
+++ client/src/pages/Downloads.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Downloads = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 p-8">
+      <h1 className="text-3xl font-bold mb-6">Download Center</h1>
+      {/* Downloads interface will go here */}
+    </div>
+  );
+};
+
+export default Downloads; 
