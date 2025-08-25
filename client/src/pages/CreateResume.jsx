425375222276d91b8a305aa24478f2eac1544b5a
Louis Nguyen <louis.nguyen550@gmail.com>
2025-08-25 19:35:51 -0400

    building form

--- client/src/pages/CreateResume.jsx
+++ client/src/pages/CreateResume.jsx
@@ -1,10 +1,147 @@
-import { Navbar } from "../components/Navbar";
+import { Navbar } from '../components/Navbar';
 
 export const CreateResume = () => {
 	return (
 		<div className="min-h-screen">
-      <Navbar/>
-      
+			<Navbar />
+			<div>
+				<form class="max-w-md pl-10 py-10">
+					<div class="grid md:grid-cols-2 md:gap-6">
+						<div class="relative z-0 w-full mb-5 group">
+							<input
+								type="text"
+								name="floating_first_name"
+								id="floating_first_name"
+								class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer"
+								placeholder=" "
+								required
+							/>
+							<label
+								for="floating_first_name"
+								class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-emerald-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+							>
+								First name
+							</label>
+						</div>
+						<div class="relative z-0 w-full mb-5 group">
+							<input
+								type="text"
+								name="floating_last_name"
+								id="floating_last_name"
+								class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer"
+								placeholder=" "
+								required
+							/>
+							<label
+								for="floating_last_name"
+								class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-emerald-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+							>
+								Last name
+							</label>
+						</div>
+					</div>
+					<div class="relative z-0 w-full mb-5 group">
+						<input
+							type="email"
+							name="floating_email"
+							id="floating_email"
+							class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer"
+							placeholder=" "
+							required
+						/>
+						<label
+							for="floating_email"
+							class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-emerald-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+						>
+							Email address
+						</label>
+					</div>
+					<div class="grid md:grid-cols-2 md:gap-6">
+						<div class="relative z-0 w-full mb-5 group">
+							<input
+								type="tel"
+								pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+								name="floating_phone"
+								id="floating_phone"
+								class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer"
+								placeholder=" "
+								required
+							/>
+							<label
+								for="floating_phone"
+								class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-emerald-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+							>
+								Phone number
+							</label>
+						</div>
+						<div class="relative z-0 w-full mb-5 group">
+							<input
+								type="text"
+								name="floating_linkedin"
+								id="floating_linkedin"
+								class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer"
+								placeholder=" "
+								required
+							/>
+							<label
+								for="floating_linkedin"
+								class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-emerald\-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+							>
+								LinkedIn
+							</label>
+						</div>
+					</div>
+					<div class="relative z-0 w-full mb-5 group">
+            <h2>Technical Skills</h2>
+						<input
+							type="text"
+							name="floating_skills"
+							id="floating_skills"
+							class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer"
+							placeholder="Langauges"
+							required
+						/>
+						<input
+							type="text"
+							name="floating_skills"
+							id="floating_skills"
+							class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer"
+							placeholder="Libraries and Frameworks"
+							required
+						/>
+						<input
+							type="text"
+							name="floating_skills"
+							id="floating_skills"
+							class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer"
+							placeholder="Developer/Design Tools"
+							required
+						/>
+					</div>
+					<div class="relative z-0 w-full mb-5 group">
+						<input
+							type="text"
+							name="floating_eduction"
+							id="floating_education"
+							class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer"
+							placeholder=" "
+							required
+						/>
+						<label
+							for="floating_education"
+							class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-emerald-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+						>
+							Education
+						</label>
+					</div>
+					<button
+						type="submit"
+						class="text-white bg-emerald-700 hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
+					>
+						Submit
+					</button>
+				</form>
+			</div>
 		</div>
 	);
 };
