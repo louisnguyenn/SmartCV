919a6d6a70bb9c66c58177c35687e7ca29b58137
Louis Nguyen <louis.nguyen550@gmail.com>
2025-09-01 20:27:13 -0400

    adjusted spacing

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,8 +7,11 @@ export const Navbar = () => {
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 				<div className="flex justify-between items-center h-16">
 					<Link to="/">
-						<h1 className="flex gap-2 items-center text-3xl font-extrabold tracking-tight text-gray-700 hover:scale-103 transition-all duration-300 drop-shadow-sm">
-							<ArrowLeft size={28}/><span className="text-emerald-600">Smart</span>CV
+						<h1 className="flex gap-2 items-center text-3xl hover:scale-103 transition-all duration-300 drop-shadow-smtracking-tight">
+							<ArrowLeft size={28} />
+							<div className="font-extrabold text-gray-700 ">
+								<span className="text-emerald-600">Smart</span>CV
+							</div>
 						</h1>
 					</Link>
 				</div>
